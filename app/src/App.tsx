a1ec2537b57f37beaea5197089c3903edef62a14
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2023-06-11 23:14:25 -0300

    Corrections and Test
    foi removido a utilizaçao de 'use Styled Components' e foi colocado um component para testar a utilização do bootstrap.
    
    npx generate-react-cli component component-name command now generates valid components

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,28 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Stack from './components/Stack/Stack';
 // or less ideally
 import { Button } from 'react-bootstrap';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Stack></Stack>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <body>
+        <NavBar></NavBar>
+      </body>
     </div>
   );
 }
