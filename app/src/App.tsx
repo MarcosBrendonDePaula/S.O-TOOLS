1ea74dc94a5b005bbce3d57544ae36e317dca414
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2023-06-11 22:43:21 -0300

    🎊🎊 starting the project 🎊🎊

---
+++ app/src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Stack from './components/Stack/Stack';
+// or less ideally
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Stack></Stack>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
